1c9a433f5ec08fe756cafd39fd1e784e2c865d6c
vovaent <vovaent@gmail.com>
2023-04-20 01:07:17 +0300

    Masthead block dev

--- template-parts/blocks/masthead/masthead.php
+++ template-parts/blocks/masthead/masthead.php
@@ -12,16 +12,41 @@
 $fields = get_fields();
 ?>
 <section class="masthead">
-	<div class="masthead__container container">
+	<div class="container masthead__container">
+
+		<?php if ( $fields['title'] ) : ?>
+
 		<h1 class="masthead__title">
-			<?php $fields['masthead_title']; ?>
+			<?php $fields['title']; ?>
 		</h1>
 		<!-- /.masthead__title -->
 
+		<?php endif; ?>
+
+		<?php if ( $fields['main_slider'] ) : ?>
+
 		<div class="masthead__slider main-slider">
+
+			<?php foreach ( $fields['main_slider'] as $slide ) : ?>
+
 			<div class="main-slider__item main-slider-item">
 				<div class="main-slider-item__img-wrap">
-					<?php echo wp_get_attachment_image( $slider['slider_item']['slide_img'] ); ?>
+					<?php
+					echo wp_get_attachment_image(
+						$slide['slider_image'],
+						'neuigkeiten_medium',
+						false,
+						array( 'class' => 'main-slider-item__img-mob' )
+					);
+					?>
+					<?php
+					echo wp_get_attachment_image(
+						$slide['slider_image'],
+						'full',
+						false,
+						array( 'class' => 'main-slider-item__img' )
+					);
+					?>
 				</div>
 				<!-- /.main-slider-item__img-wrap -->
 
@@ -31,13 +56,13 @@ $fields = get_fields();
 						<div class="main-slider-nav__next"></div>
 					</div>
 					<!-- /.main-slider-item__nav main-slider-nav -->
+
 					<div class="main-slider-content">
 						<div class="main-slider-content__title">
-							<?php echo $slider['slider_item']['content_title']; ?>
+							<?php echo $slide['content_title']; ?>
 						</div>
 						<div class="main-slider-content__description">
-							<?php echo $slider['slider_item']['content_title']; ?>
-							<?php echo apply_filters( 'the_content', $slider['slider_item']['content_description'] ); ?>
+							<?php echo apply_filters( 'the_content', $slide['content_description'] ); ?>
 						</div>
 					</div>
 					<!-- /.main-slider-content -->
@@ -45,8 +70,14 @@ $fields = get_fields();
 				<!-- /.main-slider-item__content-wrap main-slider-content-wrap -->
 			</div>
 			<!-- /.main-slider__item main-slider-item -->
+
+			<?php endforeach; ?>
+
 		</div>
 		<!-- /.masthead__slider main-slider -->
+
+		<?php endif; ?>
+
 	</div>
 	<!-- /.masthead__container container -->
 </section>
