ec953dfee772a51387792c5d52f7ca2e3a3050b8
vovaent <vovaent@gmail.com>
2023-04-21 09:09:36 +0300

    Block Masthead template name fixed

--- template-parts/blocks/masthead/masthead.php
+++ template-parts/blocks/masthead/masthead.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Block Header Template.
+ * Block "Masthead" Template.
  *
  * @param   array $block The block settings and attributes.
  * @param   string $content The block inner HTML (empty).
@@ -12,24 +12,24 @@
 $fields = get_fields();
 ?>
 <section class="masthead">
-    <div class="container masthead__container">
-        <h1 class="masthead__title">
-            <?php echo $fields['title']; ?>
-        </h1>
-        <!-- /.masthead__title -->
+	<div class="container masthead__container">
+		<h1 class="masthead__title">
+			<?php echo $fields['title']; ?>
+		</h1>
+		<!-- /.masthead__title -->
 
-        <?php if ( $fields['main_slider'] ) : ?>
+		<?php if ( $fields['main_slider'] ) : ?>
 
-        <div class="masthead__slider main-slider">
-            <div class="main-slider__glide glide">
-                <div class="glide__track" data-glide-el="track">
-                    <ul class="glide__slides">
+		<div class="masthead__slider main-slider">
+			<div class="main-slider__glide glide">
+				<div class="glide__track" data-glide-el="track">
+					<ul class="glide__slides">
 
-                        <?php foreach ( $fields['main_slider'] as $slide ) : ?>
+						<?php foreach ( $fields['main_slider'] as $slide ) : ?>
 
-                        <li class="glide__slide">
-                            <div class="glide__img-wrap">
-                                <?php
+						<li class="glide__slide">
+							<div class="glide__img-wrap">
+								<?php
 								echo wp_get_attachment_image(
 									$slide['slider_image'],
 									'neuigkeiten_medium',
@@ -37,7 +37,7 @@ $fields = get_fields();
 									array( 'class' => 'glide__mob-img' )
 								);
 								?>
-                                <?php
+								<?php
 								echo wp_get_attachment_image(
 									$slide['slider_image'],
 									'full',
@@ -45,44 +45,44 @@ $fields = get_fields();
 									array( 'class' => 'glide__img' )
 								);
 								?>
-                            </div>
-                            <!-- /.glide__img-wrap -->
-                        </li>
-                        <!-- /.glide__slide -->
+							</div>
+							<!-- /.glide__img-wrap -->
+						</li>
+						<!-- /.glide__slide -->
 
-                        <?php endforeach; ?>
+						<?php endforeach; ?>
 
-                    </ul>
-                    <!-- /.glide__slides -->
-                </div>
-                <!-- /.glide__track -->
-            </div>
-            <!-- /. main-slider__glide glide -->
+					</ul>
+					<!-- /.glide__slides -->
+				</div>
+				<!-- /.glide__track -->
+			</div>
+			<!-- /. main-slider__glide glide -->
 
-            <div class="main-slider__content-wrap">
-                <div class="main-slider__arrows">
-                    <div class="main-slider__arrow main-slider__arrow--left"></div>
-                    <div class="main-slider__arrow main-slider__arrow--right"></div>
-                </div>
-                <!-- /.main-slider__arrows glide-nav -->
+			<div class="main-slider__content-wrap">
+				<div class="main-slider__arrows">
+					<div class="main-slider__arrow main-slider__arrow--left"></div>
+					<div class="main-slider__arrow main-slider__arrow--right"></div>
+				</div>
+				<!-- /.main-slider__arrows glide-nav -->
 
-                <div class="main-slider__content">
-                    <div class="main-slider__content-title">
-                        <?php echo $fields['content_title']; ?>
-                    </div>
-                    <div class="main-slider__content-description">
-                        <?php echo apply_filters( 'the_content', $fields['content_description'] ); ?>
-                    </div>
-                </div>
-                <!-- /.main-slider__content -->
-            </div>
-            <!-- /.main-slider__content-wrap -->
-        </div>
-        <!-- /.masthead__slider main-slider -->
+				<div class="main-slider__content">
+					<div class="main-slider__content-title">
+						<?php echo $fields['content_title']; ?>
+					</div>
+					<div class="main-slider__content-description">
+						<?php echo apply_filters( 'the_content', $fields['content_description'] ); ?>
+					</div>
+				</div>
+				<!-- /.main-slider__content -->
+			</div>
+			<!-- /.main-slider__content-wrap -->
+		</div>
+		<!-- /.masthead__slider main-slider -->
 
-        <?php endif; ?>
+		<?php endif; ?>
 
-    </div>
-    <!-- /.masthead__container container -->
+	</div>
+	<!-- /.masthead__container container -->
 </section>
 <!-- /.masthead -->
