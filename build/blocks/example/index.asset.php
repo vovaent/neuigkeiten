ea5df7258eec0f965534f07f7a8a369815974743
vovaent <vovaent@gmail.com>
2023-04-23 23:39:35 +0300

    Remove the now ignored directory build

--- build/blocks/example/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '468742f1e1d43caa88c5');
--- build/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '50a922d3b8ef991fc016');
