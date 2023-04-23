8e96772940d154669906596e6a1caf875cf81cbd
vovaent <vovaent@gmail.com>
2023-04-23 22:18:59 +0300

    Footer minor fix

--- footer.php
+++ footer.php
@@ -9,31 +9,32 @@
 ?>
 
 <footer class="footer">
-    <div class="container footer__container">
-        <div class="footer__widget footer__widget--footer-1">
-            <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-1' ) ) : ?>
-            <?php endif; ?>
-        </div>
-        <!-- /.footer__widget -->
+	<div class="container footer__container">
+		<div class="footer__widget footer__widget--footer-1">
+			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-1' ) ) : ?>
+			<?php endif; ?>
+		</div>
+		<!-- /.footer__widget -->
 
+		<div class="footer__widget footer__widget--footer-2">
+			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-2' ) ) : ?>
+			<?php endif; ?>
+		</div>
+		<!-- /.footer__widget -->
 
-        <div class="footer__widget footer__widget--footer-2">
-            <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-2' ) ) : ?>
-            <?php endif; ?>
-        </div>
-        <!-- /.footer__widget -->
-        <div class="footer__widget footer__widget--footer-3">
-            <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-3' ) ) : ?>
-            <?php endif; ?>
-        </div>
-        <!-- /.footer__widget -->
-        <div class="footer__widget footer__widget--footer-4">
-            <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-4' ) ) : ?>
-            <?php endif; ?>
-        </div>
-        <!-- /.footer__widget -->
-    </div>
-    <!-- /.container footer__container -->
+		<div class="footer__widget footer__widget--footer-3">
+			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-3' ) ) : ?>
+			<?php endif; ?>
+		</div>
+		<!-- /.footer__widget -->
+
+		<div class="footer__widget footer__widget--footer-4">
+			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'footer-4' ) ) : ?>
+			<?php endif; ?>
+		</div>
+		<!-- /.footer__widget -->
+	</div>
+	<!-- /.container footer__container -->
 </footer>
 <!-- /.footer -->
 
